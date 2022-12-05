ab164c19d0a36d5ddac8b234c09fd68124487c2f
2Tough <vainferro@gmail.com>
2022-12-05 15:14:59 +0300

    Working code for explotion sprite.

--- script.js
+++ script.js
@@ -3,28 +3,51 @@ const ctx = canvas.getContext('2d')
 canvas.width = 500
 canvas.height = 700
 const explosions = []
+let canvasPosition = canvas.getBoundingClientRect()
 
-
-class Exploxion {
+class Explosion {
     constructor(x,y){
-        this.x = x
-        this.y = y
+
         this.spriteWidth = 200
         this.spriteHeight = 179
-        this.width = this.spriteWidth/0.5
-        this.height = this.spriteHeight*0.5
+        this.width = this.spriteWidth*0.7
+        this.height = this.spriteHeight*0.7
+        this.x = x - this.width/2
+        this.y = y - this.height/2
         this.image = new Image()
-        this.image.src = './images.boom.png'
+        this.image.src = './images/boom.png'
         this.frame = 0
+        this.timer = 0
     }
     update() {
-        this.frame++
+        this.timer++
+        if(this.timer % 10 === 0) {
+            this.frame++
+        }
     }
     draw() {
         ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
     }
 }
 
-window.addEventListener('click', function() {
-    ctx.fillRect(x, y, width, height)
+window.addEventListener('click', function(e) {
+    let positionX = e.x - canvasPosition.left
+    let positionY = e.y - canvasPosition.top
+    explosions.push(new Explosion(positionX, positionY))
+    console.log(explosions)
+
 })
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    for (let i = 0 ; i < explosions.length ; i++) {
+        explosions[i].update()
+        explosions[i].draw()
+        if(explosions[i].frame > 5) {
+            explosions.splice(i, 1)
+            i--
+        }
+    }
+    requestAnimationFrame(animate)
+}
+animate()
