4431c0a0c7d1e1ff1c04dd468316eb78df075917
2Tough <vainferro@gmail.com>
2022-12-05 14:39:52 +0300

    Added class.

---
+++ script.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementById("canvas1")
+const ctx = canvas.getContext('2d')
+canvas.width = 500
+canvas.height = 700
+const explosions = []
+
+
+class Exploxion {
+    constructor(x,y){
+        this.x = x
+        this.y = y
+        this.spriteWidth = 200
+        this.spriteHeight = 179
+        this.width = this.spriteWidth/0.5
+        this.height = this.spriteHeight*0.5
+        this.image = new Image()
+        this.image.src = './images.boom.png'
+        this.frame = 0
+    }
+    update() {
+        this.frame++
+    }
+    draw() {
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
+    }
+}
+
+window.addEventListener('click', function() {
+    ctx.fillRect(x, y, width, height)
+})
